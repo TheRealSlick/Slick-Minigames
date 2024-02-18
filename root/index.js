7300b240314e132af30060385d171368e64e1071
TheRealSlick <wadams0107@gmail.com>
2024-02-18 11:38:23 -0800

    made some layouts

---
+++ root/index.js
@@ -0,0 +1,26 @@
+// Classes
+class Points{
+    constructor(game_mode, pointAmount) {
+        // Properties
+        this.game_mode = game_mode
+        this.pointAmount = pointAmount
+
+        
+    }
+
+    // Methods
+    awardPoints() {
+        
+    }
+
+    removePoints() {
+        
+    }
+}
+
+
+// Variables
+
+//Functions
+
+// Invokes
