c7477d750b59c7c55f9c2ae147e9d3cc36f1869e
TheRealSlick <wadams0107@gmail.com>
2024-02-22 08:24:45 -0800

    added random tile generator/randomizer

--- simons-says/simon-says-script.js
+++ simons-says/simon-says-script.js
@@ -1,15 +1,17 @@
 class SimonSays {
     constructor() {
-        // Properties
+        // States
         this.Started = true
         this.Paused = false
         this.Ended = false
+
+        // Properties
         this.differentColoredTile = false
-        // this.currentBoard = '2x2'
         this.Level = 1
         this.maxLevel = 9
         this.tilesAmount = 2
         this.currentBoard = { board: 2 }
+
 
         // Color Values
         this.easyColor = 100
@@ -17,6 +19,8 @@ class SimonSays {
         this.hardColor = 15
         this.extremeColor = 13
 
+        // Arrays
+        this.tilesArray = []
 
 
 
@@ -34,15 +38,23 @@ class SimonSays {
 
     }
 
+
+    // Color Change Methods
     randomRGBValue(min, max) {
         var minRGB = Math.floor(min)
         var maxRBG = Math.floor(max)
 
-        return Math.ceil(Math.random() * (max - min) + min)
+        return Math.ceil(Math.random() * (maxRBG - minRGB) + min)
     }
 
     rgb(r, g, b) {
         return "rgb(" + r + "," + g + "," + b + ")";
+    }
+
+    randomTileColor(array) {
+        let randomTile = array[Math.floor(Math.random() * array.length)]
+        console.log(randomTile);
+        return randomTile
     }
 
     createBoard() {
@@ -51,8 +63,11 @@ class SimonSays {
                 for (let index = 0; index < value ** 2; index++) {
                     const container = document.getElementById('gametiles')
                     const element = document.createElement('button')
+                    element.id = 'colored-button'
                     element.style.borderRadius = '10px'
-                    element.style.backgroundColor = this.rgb(this.randomRGBValue(2, 255), 0, 0)
+                    // element.style.backgroundColor = this.rgb(this.randomRGBValue(2, 255), 0, 0)
+                    this.tilesArray.push(element)
+                    console.log(this.tilesArray);
                     container.appendChild(element)
                 }
             }
@@ -67,10 +82,13 @@ class SimonSays {
 }
 
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
     let game = new SimonSays()
 
     game.createBoard()
+    game.randomTileColor(game.tilesArray)
 
 })
 
