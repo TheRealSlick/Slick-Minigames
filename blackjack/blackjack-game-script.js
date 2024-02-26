eee00ff8292aa174455014a3014da8c0ef96d123
TheRealSlick <wadams0107@gmail.com>
2024-02-25 16:19:13 -0800

    working...

--- blackjack/blackjack-game-script.js
+++ blackjack/blackjack-game-script.js
@@ -1,5 +1,29 @@
+// Arrays
 var currentDeck = []
 
+// Bools
+var playerTurn = true
+var dealerTurn = false
+var Started = false
+var Paused = false
+var Ended = false
+var Intiated = false
+var HIT = false
+var STAY = false
+
+// Values
+var playerScore = 0
+var dealerScore = 0
+
+var maxPlayerScore = 21
+var maxDealerScore = 21
+var dealerStandScore = 17
+
+// Elements
+var hitButton = document.getElementById('hit')
+var stayButton = document.getElementById('stay')
+
+// Functions
 function createDeck(deck) {
     var deckValues = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'king', 'queen', 'jack']
     var deckSuitType = ['of_spades', 'of_hearts', 'of_diamonds', 'of_clubs']
@@ -9,7 +33,6 @@ function createDeck(deck) {
             deck.push(deckValues[i] + '_' + deckSuitType[j])
         }
     }
-    console.log(deck);
 }
 
 function shuffleDeck(deck) {
@@ -20,48 +43,65 @@ function shuffleDeck(deck) {
         deck[i] = deck[shuffle]
         deck[shuffle] = card
     }
-    console.log(deck);
+}
+
+function deckPoints() {
+    for (let i = 0; i < currentDeck.length; i++) {
+        var card = currentDeck[i]
+        if (card.includes('')) {
+            console.log(true);
+        }
+    }
 }
 
 createDeck(currentDeck)
 shuffleDeck(currentDeck)
-var r = currentDeck
-console.log(Math.floor(Math.random() * r.length));
 
 
-function getCard() {
-    var array = document.getElementById('dealer-cards')
-    // var a = array.getElementsByTagName('img')
-    // var b = array.getElementsByTagName('test')
-    // console.log(array);
-    // for (let i = 0; i < a.length; i++) {
-    //     const element = a[i]
-    //     if (element.id == 'test') {
-    //         console.log(element);
-    //     }
-    // }
-
-    for (let i = 0; i < 1; i++) {
-        var newcard = document.createElement("img")
-        var randomCard = Math.floor(Math.random() * currentDeck.length)
-        var card;
-        for (let j = 0; j < currentDeck.length; j++) {
-            const element = currentDeck[j];
-            if (j == randomCard) {
-                card = element
+function dealCard() {
+    var card;
+    if (playerTurn == true) {
+        var playerCards = document.getElementById('your-cards')
+        for (let i = 0; i < 1; i++) {
+            var playerNewCard = document.createElement("img")
+            var playerRandomCard = Math.floor(Math.random() * currentDeck.length)
+            for (let j = 0; j < currentDeck.length; j++) {
+                const element = currentDeck[j];
+                if (j == playerRandomCard) {
+                    card = element
+                }
             }
-            
+            playerNewCard.src = 'blackjackassets/DeckOfCards/' + card + '.png'
+            playerNewCard.alt = 'card'
+            playerCards.appendChild(playerNewCard)
+            console.log(playerNewCard);
         }
-        newcard.src = 'blackjackassets/DeckOfCards/' + card + '.png'
-        newcard.alt = 'card'
-        array.appendChild(newcard)
-        console.log(newcard);
+    }
 
-
+    if (!playerTurn && dealerTurn == true) {
+        var dealerCards = document.getElementById('dealer-cards')
+        for (let i = 0; i < 1; i++) {
+            var dealerNewCards = document.createElement("img")
+            var dealerRandomCard = Math.floor(Math.random() * currentDeck.length)
+            for (let j = 0; j < currentDeck.length; j++) {
+                const element = currentDeck[j];
+                if (j == dealerRandomCard) {
+                    card = element
+                }
+            }
+            dealerNewCards.src = 'blackjackassets/DeckOfCards/' + card + '.png'
+            dealerNewCards.alt = 'card'
+            dealerCards.appendChild(dealerNewCards)
+            console.log(dealerNewCards);
+        }
     }
 }
 
 
+
 document.addEventListener('DOMContentLoaded', () => {
-    getCard()
+    dealCard()
+    deckPoints()
 })
+
+
